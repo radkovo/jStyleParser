9a4e59636b4401d8d3278a8847c718101c3bc64a
Radek Burget <radkovo@users.sf.net>
2009-02-09 19:59:56 +0000

    Vendor-specific properties supported and tested

--- src/test/GrammarRecovery2.java
+++ src/test/GrammarRecovery2.java
@@ -2,31 +2,18 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Date;
-import java.util.Map;
-
-import junit.framework.Assert;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.tidy.Tidy;
 
 import cz.vutbr.web.css.CSSException;
 import cz.vutbr.web.css.CSSFactory;
-import cz.vutbr.web.css.NodeData;
-import cz.vutbr.web.css.RuleMedia;
-import cz.vutbr.web.css.RuleSet;
 import cz.vutbr.web.css.StyleSheet;
 import cz.vutbr.web.css.TermFactory;
-import cz.vutbr.web.css.TermList;
-import cz.vutbr.web.css.CSSProperty.FontFamily;
-import cz.vutbr.web.domassign.Analyzer;
 
 public class GrammarRecovery2 {
 	private static Logger log = LoggerFactory.getLogger(GrammarRecovery1.class);
@@ -55,36 +42,36 @@ public class GrammarRecovery2 {
 	public void vendorSpecificUnderscore() throws IOException, CSSException 
 	{
 		StyleSheet ss = CSSFactory.parse(TEST_DECL1A);
-		assertEquals("Both properties are accepted (second one is extension)", 2, ss.size());
+		assertEquals("Both properties are accepted (second one is extension)", 2, ss.get(0).size());
         ss = CSSFactory.parse(TEST_DECL1B);
-        assertEquals("Both properties are accepted (first one is extension)", 2, ss.size());
+        assertEquals("Both properties are accepted (first one is extension)", 2, ss.get(0).size());
 	}
 
     @Test
     public void vendorSpecificDash() throws IOException, CSSException 
     {
         StyleSheet ss = CSSFactory.parse(TEST_DECL2A);
-        assertEquals("Both properties are accepted (second one is extension)", 2, ss.size());
+        assertEquals("Both properties are accepted (second one is extension)", 2, ss.get(0).size());
         ss = CSSFactory.parse(TEST_DECL2B);
-        assertEquals("Both properties are accepted (first one is extension)", 2, ss.size());
+        assertEquals("Both properties are accepted (first one is extension)", 2, ss.get(0).size());
     }
     
     @Test
     public void invalidCharColon() throws IOException, CSSException 
     {
         StyleSheet ss = CSSFactory.parse(TEST_DECL3A);
-        assertEquals("One property is accepted (second one is invalid)", 1, ss.size());
+        assertEquals("One property is accepted (second one is invalid)", 1, ss.get(0).size());
         ss = CSSFactory.parse(TEST_DECL3B);
-        assertEquals("One property is accepted (first one is invalid)", 1, ss.size());
+        assertEquals("One property is accepted (first one is invalid)", 1, ss.get(0).size());
     }
     
     @Test
     public void invalidCharAsterisk() throws IOException, CSSException 
     {
         StyleSheet ss = CSSFactory.parse(TEST_DECL4A);
-        assertEquals("One property is accepted (second one is invalid)", 1, ss.size());
+        assertEquals("One property is accepted (second one is invalid)", 1, ss.get(0).size());
         ss = CSSFactory.parse(TEST_DECL4B);
-        assertEquals("One property is accepted (first one is invalid)", 1, ss.size());
+        assertEquals("One property is accepted (first one is invalid)", 1, ss.get(0).size());
     }
 
 }
