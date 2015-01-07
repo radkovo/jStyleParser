59389c76af2ac473b80e873f3b6c068bc7c03b89
Radek Burget <burgetr@fit.vutbr.cz>
2015-01-07 23:58:27 +0100

    NetwrokProcessor interface and default implementation

---
+++ src/main/java/cz/vutbr/web/css/NetworkProcessor.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package cz.vutbr.web.css;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * A network processor that is able to obtain an input stream from a URL.
+ * 
+ * @author burgetr
+ */
+public interface NetworkProcessor
+{
+
+    /**
+     * Fetches the resource with the given URL and creates a stream containing
+     * the resource contents.
+     * 
+     * @param url Resource URL.
+     * @return input stream that reads resource contents
+     * @throws IOException
+     */
+    public InputStream fetch(URL url) throws IOException;
+    
+}
---
+++ src/main/java/cz/vutbr/web/csskit/DefautNetworkProcessor.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package cz.vutbr.web.csskit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.zip.GZIPInputStream;
+
+import cz.vutbr.web.css.NetworkProcessor;
+
+/**
+ * Default implementation of the NetworkProcessor that is used when no other
+ * implementation is provided. This implementation is based on the java built-in 
+ * URLConnection mechanism.
+ * 
+ * @author burgetr
+ */
+public class DefautNetworkProcessor implements NetworkProcessor
+{
+
+    @Override
+    public InputStream fetch(URL url) throws IOException
+    {
+        URLConnection con = url.openConnection();
+        InputStream is;
+        if ("gzip".equalsIgnoreCase(con.getContentEncoding()))
+            is = new GZIPInputStream(con.getInputStream());
+        else
+            is = con.getInputStream();
+        
+        return is;
+    }
+
+}
