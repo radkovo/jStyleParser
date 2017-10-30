a70ce2bb0e740a52faa863526e38d16719f01450
czaurel <czaurel@gmail.com>
2017-10-30 13:26:21 +0100

    #78: Strongly typed read access to RuleFontFace properties. Included: font-family, src, font-weight, font-style.

--- src/main/java/cz/vutbr/web/csskit/RuleFontFaceImpl.java
+++ src/main/java/cz/vutbr/web/csskit/RuleFontFaceImpl.java
@@ -5,8 +5,17 @@
  */
 package cz.vutbr.web.csskit;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.fit.net.DataURLHandler;
+
+import cz.vutbr.web.css.CSSProperty.FontStyle;
+import cz.vutbr.web.css.CSSProperty.FontWeight;
 import cz.vutbr.web.css.Declaration;
 import cz.vutbr.web.css.RuleFontFace;
+import cz.vutbr.web.css.Term;
+import cz.vutbr.web.css.TermURI;
 
 /**
  * Wrap of declarations bound with a font specification
@@ -16,11 +25,61 @@ import cz.vutbr.web.css.RuleFontFace;
 public class RuleFontFaceImpl extends AbstractRuleBlock<Declaration> implements RuleFontFace 
 {
     
+	private static final String PROPERTY_FONT_FAMILY_NAME = "font-family";
+	private static final String PROPERTY_SOURCE = "src";
+	private static final String PROPERTY_FONT_STYLE = "font-style";
+	private static final String PROPERTY_FONT_WEIGHT = "font-weight";
+	
     protected RuleFontFaceImpl() 
     {
         super();
     }
     
+    @Override
+	public String getFontFamily() {
+    	return getStringValue(PROPERTY_FONT_FAMILY_NAME);
+	}
+
+	@Override
+	public URL getSource() {
+		TermURI urlstring = getTermURI(PROPERTY_SOURCE);
+        URL url = null;
+        try {
+        	url = DataURLHandler.createURL(urlstring.getBase(), urlstring.getValue());
+        } catch (MalformedURLException e) {
+        }
+        
+        return url;
+	}
+
+	@Override
+	public FontStyle getFontStyle() {
+		String strValue = getStringValue(PROPERTY_FONT_STYLE);
+		if (strValue == null) {
+			return null;
+		}
+		
+		try{
+			return FontStyle.valueOf(strValue.toUpperCase());
+		} catch (IllegalArgumentException e){
+			return null;
+		}
+	}
+
+	@Override
+	public FontWeight getFontWeight() {
+		String strValue = getStringValue(PROPERTY_FONT_WEIGHT);
+		if (strValue == null) {
+			return null;
+		}
+		
+		try{
+			return FontWeight.valueOf(strValue.toUpperCase());
+		} catch (IllegalArgumentException e){
+			return null;
+		}
+	}
+	
     @Override 
     public String toString() 
     {
@@ -40,5 +99,51 @@ public class RuleFontFaceImpl extends AbstractRuleBlock<Declaration> implements 
     
         return sb.toString();
     }
-
+    
+    private String getStringValue(String propertyName) {
+        Declaration decl = getDeclaration(propertyName);
+    	if (decl == null) {
+    		return null;
+    	}
+    	
+    	Term<?> term= decl.get(0);
+    	if (term == null) {
+    		return null;
+    	}
+    	
+    	Object value = term.getValue();
+    	if (!(value instanceof String)) {
+    		return null;
+    	}
+    	
+    	return (String)value;
+    }
+    
+    private TermURI getTermURI(String propertyName) {
+    	Declaration decl = getDeclaration(propertyName);
+    	if (decl == null) {
+    		return null;
+    	}
+    	
+    	Term<?> term= decl.get(0);
+    	if (term == null) {
+    		return null;
+    	}
+    	
+    	if (!(term instanceof TermURI)) {
+    		return null;
+    	}
+    	
+    	return (TermURI)term;
+    }
+    
+    private Declaration getDeclaration(String property) {
+    	for (Declaration decl : list) {
+			if (property.equals(decl.getProperty())) {
+				return decl;
+			}
+		}
+    	
+    	return null;
+    }
 }
